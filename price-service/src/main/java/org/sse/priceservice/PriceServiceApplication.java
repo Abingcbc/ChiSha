265cfacfd83cbb3c585fe69425a1dfa0abfa2aaa
908115501 <908115501@qq.com>
2019-12-25 16:21:10 +0800

    price-service

---
+++ price-service/src/main/java/org/sse/priceservice/PriceServiceApplication.java
@@ -0,0 +1,13 @@
+package org.sse.priceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriceServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PriceServiceApplication.class, args);
+    }
+
+}
