a6dab7c0d3891ca2f0a5fab5a50fe8d7263f3f36
Abingcbc <19990626.love@163.com>
2019-12-25 23:35:36 +0800

    auto test

--- user-service/src/main/java/org/sse/userservice/mapper/UserMapper.java
+++ user-service/src/main/java/org/sse/userservice/mapper/UserMapper.java
@@ -13,60 +13,19 @@ public interface UserMapper {
 
     /**
      * get user auth info by username when login
-     * @param username user's name
+     * @param email user's email
      * @return user auth info
      */
-    @Select("select * from user where username = #{username}")
-    User getUserByUsername(@Param("username") String username);
+    @Select("select * from user where email = #{email}")
+    User getUserByEmail(@Param("email") String email);
 
     /**
      * create new user
-     * @param username username
-     * @param password password
-     * @param email email
-     * @param avatarUrl avatar url
+     * @param user Object of user
      * @return number of affected rows
      */
-    @Insert("insert into user(username, password, email, avatar_url, like_num)" +
-            " values(#{username}, #{password}, #{email}, #{avatarUrl}, 0);")
-    int createNewUser(@Param("username") String username,
-                      @Param("password") String password,
-                      @Param("email") String email,
-                      @Param("avatarUrl") String avatarUrl);
+    @Insert("INSERT INTO user(NICKNAME, PHONE, EMAIL, IS_AUTHORIZED, GENDER, PASSWORD, BORN_PLACE, AGE) \n" +
+            "VALUES (#{nickname}, #{phone}, #{email}, #{isAuthorized}, #{gender}, #{password}, #{age});")
+    int createNewUser(@Param("user") User user);
 
-    /**
-     * update user's password
-     * @param username username
-     * @param password new password
-     * @return number of affected rows
-     */
-    @Update("update user\n" +
-            "set password = #{password}, email = #{email}\n" +
-            "where username = #{username}\n")
-    int updatePassword(@Param("username") String username,
-                       @Param("password") String password);
-
-    /**
-     * update user's Email
-     * @param username username
-     * @param email new email
-     * @return number of affected rows
-     */
-    @Update("update user\n" +
-            "set email = #{email}\n" +
-            "where username = #{username}\n")
-    int updateEmail(@Param("username") String username,
-                    @Param("email") String email);
-
-    /**
-     * update user's avatar
-     * @param username username
-     * @param avatarUrl new avatar url
-     * @return number of affected rows
-     */
-    @Update("update user\n" +
-            "set avatar_url = #{avatarUrl}\n" +
-            "where username = #{username}\n")
-    int updateAvatar(@Param("username") String username,
-                     @Param("avatarUrl") String avatarUrl);
 }
