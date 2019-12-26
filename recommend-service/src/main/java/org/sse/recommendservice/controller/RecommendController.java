d64ec6d6642483b588ac8f52d43d035a3277ce26
908115501 <908115501@qq.com>
2019-12-26 14:44:41 +0800

    recommend service config

--- recommend-service/src/main/java/org/sse/recommendservice/controller/RecommendController.java
+++ recommend-service/src/main/java/org/sse/recommendservice/controller/RecommendController.java
@@ -1,7 +1,14 @@
 package org.sse.recommendservice.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * @author cbc
  */
 public class RecommendController {
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "hello";
+    }
 }
