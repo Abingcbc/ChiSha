26f0b0b3be2e74bb48fc5a461624fdcde701834c
908115501 <908115501@qq.com>
2019-12-26 16:59:46 +0800

    Update RecommendController.java

--- recommend-service/src/main/java/org/sse/recommendservice/controller/RecommendController.java
+++ recommend-service/src/main/java/org/sse/recommendservice/controller/RecommendController.java
@@ -1,11 +1,14 @@
 package org.sse.recommendservice.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author cbc
  */
+@RestController
 public class RecommendController {
     @GetMapping("/hello")
     public String sayHello(){
