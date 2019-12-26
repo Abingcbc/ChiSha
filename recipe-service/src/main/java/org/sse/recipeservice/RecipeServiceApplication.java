69233263a0a33869c9a3aa7affc19e76d0d69a27
908115501 <908115501@qq.com>
2019-12-26 20:46:42 +0800

    recipe

---
+++ recipe-service/src/main/java/org/sse/recipeservice/RecipeServiceApplication.java
@@ -0,0 +1,13 @@
+package org.sse.recipeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipeServiceApplication.class, args);
+    }
+
+}
---
+++ recommend-service/src/main/java/org/sse/recommendservice/controller/RecipeController.java
@@ -0,0 +1,31 @@
+package org.sse.recommendservice.controller;
+
+import com.github.pagehelper.PageInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.sse.recommendservice.model.Recipe;
+import org.sse.recommendservice.service.RecipeService;
+
+/**
+ * @author HPY
+ */
+@RestController
+@RequestMapping("/recipe")
+public class RecipeController {
+    @Autowired
+    RecipeService recipeService;
+
+//    @GetMapping("/get-list")
+//    public PageInfo<Recipe> getPageOfRecipe(@RequestParam("page-num") int pageNum,
+//                                            @RequestParam("page-size") int pageSize){
+//        return recipeService.getPageOfRecipeByDefault(pageNum, pageSize);
+//    }
+
+    @GetMapping("/hello")
+    public String getPageOfRecipe(){
+        return "hellooo";
+    }
+}
