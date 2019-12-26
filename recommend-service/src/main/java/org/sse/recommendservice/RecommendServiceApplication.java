d130f4abeae78c49aea56c8a27f931b8c76061ef
Abingcbc <19990626.love@163.com>
2019-12-26 12:44:07 +0800

    init

---
+++ recommend-service/src/main/java/org/sse/recommendservice/RecommendServiceApplication.java
@@ -0,0 +1,13 @@
+package org.sse.recommendservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecommendServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecommendServiceApplication.class, args);
+	}
+
+}
