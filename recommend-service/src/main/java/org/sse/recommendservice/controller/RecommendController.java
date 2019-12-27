5ce7d1aaf19b5637805970eeb0951d136cdb4c6f
Abingcbc <19990626.love@163.com>
2019-12-27 17:11:47 +0800

    test

--- recommend-service/src/main/java/org/sse/recommendservice/controller/RecommendController.java
+++ recommend-service/src/main/java/org/sse/recommendservice/controller/RecommendController.java
@@ -1,6 +1,5 @@
 package org.sse.recommendservice.controller;
 
-import com.oracle.tools.packager.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.http.MediaType;
