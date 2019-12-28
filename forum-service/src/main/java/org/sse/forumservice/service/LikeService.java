f7fa51e9d39c83f13ed64e86bcb7291b60c2b4d4
908115501 <908115501@qq.com>
2019-12-28 14:40:54 +0800

    check like

--- forum-service/src/main/java/org/sse/forumservice/service/LikeService.java
+++ forum-service/src/main/java/org/sse/forumservice/service/LikeService.java
@@ -1,4 +1,55 @@
 package org.sse.forumservice.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.sse.forumservice.mapper.LikeMapper;
+import org.sse.forumservice.model.UserLikePost;
+
+/**
+ * @author HPY
+ */
+@Service
 public class LikeService {
+
+    @Autowired
+    LikeMapper likeMapper;
+
+    public boolean userLikePost(long userId, long postId){
+        UserLikePost userLikePost = likeMapper.getLikeByUserAndPost(userId,postId);
+        if(userLikePost == null){
+            return likeMapper.insertLike(userId, postId);
+        }
+        else if(userLikePost.getIsLike() == 0){
+            return likeMapper.setIsLike(userId,postId,1);
+        }
+        else {
+            return false;
+        }
+    }
+
+    public boolean userUnLikePost(long userId, long postId){
+        UserLikePost userLikePost = likeMapper.getLikeByUserAndPost(userId,postId);
+        if(userLikePost == null){
+            return false;
+        }
+        else if(userLikePost.getIsLike() == 1){
+            return likeMapper.setIsLike(userId,postId,0);
+        }
+        else {
+            return false;
+        }
+    }
+
+    public boolean checkIsLike(long userId,long postId){
+        UserLikePost userLikePost = likeMapper.getLikeByUserAndPost(userId, postId);
+        if(userLikePost == null){
+            return false;
+        }
+        else if(userLikePost.getIsLike()==0){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
 }
