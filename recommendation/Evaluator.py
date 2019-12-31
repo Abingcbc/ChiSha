ef55010fa3070b17ff8a6ba2d7514b5fbf220f17
冬阳 刘 <392702533@qq.com>
2019-12-31 16:20:52 +0800

    Recommendation implemented

--- recommendation/Evaluator.py
+++ recommendation/Evaluator.py
@@ -1,7 +1,10 @@
 import numpy as np
 from urllib.request import urlopen
 import pandas as pd
+from tensorflow.keras.models import load_model
+from deepctr.layers import custom_objects
 from deepctr.models import DIN,DeepFM
+from ModelEncoder import ModelEncoder
 from deepctr.inputs import SparseFeat,VarLenSparseFeat,DenseFeat,get_feature_names
 from sklearn.preprocessing import LabelEncoder, MinMaxScaler
 from tensorflow.python.keras.preprocessing.sequence import pad_sequences
@@ -16,16 +19,20 @@ class Evaluator:
 	
 	def load_encoder(self, path :str):
 		self.encoder = pickle.load(path)
-	
+		self.encoder.eva()
+		
 	def load_model(self, path: str):
-		self.model = DIN()
-		self.model.load_weights(path)
+		feature_columns = self.encoder.getFeatureColumns()
+		self.model = load_model(path, custom_objects)
 	
 	def update(self):
 		url = ''
-		data_source = urlopen(url)
-		self.recipeDomain = pd.read_csv(data_source)
+		self.recipeDomain = pd.read_csv(url)
+		self.encoder.encode(self.recipeDomain)
 		
-	def evaluate(self, user_domain :dict):
-		#Cartesian product
-	
+	def evaluate(self, user_domain: dict):
+		pd_user_domain = pd.DataFrame(user_domain, copy=True)
+		cartesian = pd.merge(pd_user_domain, self.recipeDomain, how='outer')
+		model_input = {name: cartesian[name].values for name, _ in self.encoder.fixed_sparse_dict + self.encoder.var_sparse_dict}
+		pred_ans = self.model.predict(model_input, model_input)
+		return pred_ans
--- recommendation/Trainer.py
+++ recommendation/Trainer.py
@@ -3,43 +3,69 @@ from urllib.request import urlopen
 import pandas as pd
 from ModelEncoder import ModelEncoder
 from deepctr.models import DIN, DeepFM
+from tensorflow.keras.models import save_model
 from deepctr.inputs import SparseFeat, VarLenSparseFeat, DenseFeat, get_feature_names
 from sklearn.preprocessing import LabelEncoder, MinMaxScaler
 from tensorflow.python.keras.preprocessing.sequence import pad_sequences
 import pickle
+import copy
+
 
 class Trainer:
 	def __init__(self):
 		self.data = None
 		self.encoder = None
 		self.model = None
+		# number of positive samples
+		self.num_pos = None
+		self.recipeDomain = None
 		
-	def loadData(self, data_source: str):
-		self.data = pd.read_csv(data_source)
+	def loadData(self, url: str):
+		self.data = pd.read_csv(url)
+		self.recipeDomain = pd.read_csv(url)
+		self.num_pos = self.data.shape[0]
 		self.encoder = ModelEncoder()
 		self.encoder.train()
 	
 	def preProcessData(self):
-		self.encoder.encode(self.data)
+		self.data = self.encoder.encode(self.data)
+		self.data['result'] = [1] * self.num_pos
+		
+		self.recipeDomain = self.encoder.encode(self.recipeDomain)
+
+		self.build_negative_data()
 		
 	def buildModel(self):
-		feature_columns = []
-		for feat, value in self.encoder.fixed_sparse_dict:
-			feature_columns.append(SparseFeat(feat, vocabulary_size=value[0], embedding_dim=value[1]))
-		for feat, value in self.encoder.var_sparse_dict:
-			feature_columns.append(VarLenSparseFeat(feat, maxlen=value[0], vocabulary_size=value[1], embedding_dim=value[2], embedding_name=value[3]))
-		
-		self.model = DIN(feature_columns, self.model.behavior_feature_list)
+		feature_columns = self.encoder.getFeatureColumns()
+		self.model = DIN(feature_columns, self.encoder.behavior_list)
 		self.model.compile('adam', 'binary_crossentropy', metrics=['binary_crossentropy'])
-		
+	
 	def train(self):
 		model_input = {}
 		for feat, _ in self.encoder.fixed_sparse_dict + self.encoder.var_sparse_dict:
 			model_input[feat] = self.data[feat]
+		history = self.model.fit(model_input, self.data['gt'].values, batch_size=256, epochs=10, verbose=2, validation_split=0.2, shuffle = True)
+		
 	def dump_model(self, path: str):
-		self.model.save_weights(path)
+		save_model(self.model, path)
 	
 	def dump_encoder(self, path: str):
 		pickle.dump(self.encoder, self, protocol=4)
-		
 	
+	def update(self):
+		url = ''
+		self.recipeDomain = pd.read_csv(url)
+		self.encoder.encode(self.recipeDomain)
+
+	def build_negative_data(self):
+		for i in range(self.num_pos):
+			record = self.data.iloc[i]
+			clicked_set = record['hist_recipe'] + record['recipe'][i]
+			for j in self.recipeDomain.shape[0]:
+				if self.recipeDomain.iloc[j]['recipe'] not in clicked_set:
+					# valid unclicked combination
+					for feat in record.columns:
+						if feat in self.recipeDomain.columns:
+							record[feat] = self.recipeDomain.iloc[j][feat]
+					record['result'] = 0
+					self.data.append(record)
